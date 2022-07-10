def98383e7080604836605b681c85f03eee7e4bd
NisanurCandan <candannisanurr@gmail.com>
2022-07-10 21:33:04 +0300

    Login entegrasyon yapıldı.

---
+++ Login(MVC)/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using Login_MVC_.Models;
+
+namespace Login_MVC_.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        void connectionString()
+        {
+            con.ConnectionString = "data source=DESKTOP-VKRSMAH; database=WPF; integrated security=SSPI;";
+        }
+        [HttpPost]
+        [ActionName("Verify")]
+        public ActionResult Verify(string name, string password) 
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from tbl_login where username='" + name + "' and password='" + password + "' ";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View("Create");
+            }
+            else
+            {
+                con.Close();
+                return View("Error");
+            }
+
+        }
+    }
+}
---
+++ Login(MVC)/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Login_MVC_.Models
+{
+    public class Account
+    {
+        //[DisplayName("Name")]
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+    }
+}
